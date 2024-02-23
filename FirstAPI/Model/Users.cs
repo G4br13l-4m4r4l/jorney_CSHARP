9732a2ebe2ee2c963005ccb8e4021813d5f9df7b
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-02-23 09:54:48 -0300

    recently study

---
+++ FirstAPI/Model/Users.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponenteModel.DataAnnotations;
+using System.ComponenteModel.DataAnnotations.Schema;
+
+namespace FirstAPI.Model
+{
+    [Table("users")]
+    public class Users
+    {
+        [Key]
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Users(int id, string name, int age)
+        {
+            this.id = id ?? throw new ArgumentNullException(nameof(name));
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
